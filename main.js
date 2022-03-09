4f7cf6902095a9db7c21bc256f848769e8a79633
eelizac <68539632+eelizac@users.noreply.github.com>
2022-03-09 13:56:32 +1100

    Added more animations and fixed up section heights

--- main.js
+++ main.js
@@ -6,5 +6,18 @@ const slider = document.querySelector('.slider');
 const tl = new TimelineMax();
 
 tl.fromTo(hero,1, {height:"0%"},{height:"80%", ea : Power2.easeInOut})
-.fromTo(slider, 0.7, {x:"-100%"}, {x:"0%", ease:Power2.easeInOut})
-.fromTo(headline, 0.5, {opacity:0, x:30}, {opacity: 1, x:0}, "-=0.5");
+.fromTo(hero,1.2, {width:"100%"},{width:"90%", ea : Power2.easeInOut})
+.fromTo(slider, 0.7, {x:"-100%"}, {x:"0%", ease:Power2.easeInOut}, "-=1.2")
+.fromTo(headline, 0.5, {opacity:0, x:30}, {opacity: 1, x:0}, "-=0.5"); 
+
+
+window.addEventListener('scroll',()=>{
+    let content = document.querySelector('.intro');
+    let contentPosition = content.getBoundingClientRect().top; 
+    let screenPosition = window.innerHeight/1.5; 
+    if (contentPosition < screenPosition) {
+        content.classList.add('active');
+    } else {
+        content.classList.remove('active')
+    }
+});
